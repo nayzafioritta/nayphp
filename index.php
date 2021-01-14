3a11c0ab103e16b1c954805b1cc8eaed2fb0fb0b
nayzafioritta <nayzafioritta@gmail.com>
2021-01-14 15:03:50 -0300

    link para zap

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
-<p>Olá, seja bem-vindo!</p>
+<font color="red">Olá, seja bem-vindo!</font>
 
 <p>Espero que você goste das dicas que daremos aqui.</p>
 
-<p>Fique a vontade para compartilhar com os seus amigos</p>
-
 <p>;)</p>
+
+
+<a href="https://api.whatsapp.com/send?phone=55119856258452&text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es.">Clique aqui para me chamar no whatsapp</a>
+
